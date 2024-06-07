b8c5c391d84745f354d9af96fe155003a9f57425
Rivaldo <aldolauren18@gmail.com>
2024-06-07 17:53:55 +0700

    Week 12 - Notifications

--- app/src/main/java/com/example/todoapp/util/Util.kt
+++ app/src/main/java/com/example/todoapp/util/Util.kt
@@ -1,9 +1,24 @@
 package com.example.todoapp.util
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.icu.text.CaseMap.Title
+import android.os.Build
+import android.os.Message
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.todoapp.R
 import com.example.todoapp.model.TodoDatabase
+import com.example.todoapp.view.MainActivity
 
 val DB_NAME = "newtododb"
 
@@ -26,4 +41,65 @@ val MIGRATION_2_3 = object: Migration(2,3){
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE todo ADD COLUMN is_done INTEGER default 0 not null")
     }
+}
+
+class NotificationHelper(val context: Context){
+    private val CHANNEL_ID = "todo_channel" //boleh diisi bebas
+    private val NOTIFICATION_ID = 1 //angka sembarang
+
+    companion object{
+        val REQUEST_NOTIF = 100 //angka random
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_ID,NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "Channel to publish a notification when todo created"
+            val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    //Code bikin Notification
+    fun creatNotification(title: String, message: String){
+        createNotificationChannel()
+        //Intent untuk mengarahkan user jika memencet notif maka aplikasi akan kebuka main activity
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_IMMUTABLE)
+        val icon = BitmapFactory.decodeResource(context.resources, R.drawable.todochar)
+
+        //object notif
+        val notif = NotificationCompat.Builder(context,CHANNEL_ID).setSmallIcon(R.drawable.checklist)
+            .setLargeIcon(icon)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setStyle(
+                NotificationCompat.BigPictureStyle()
+                    .bigPicture(icon)
+                    .bigLargeIcon(null)
+            )
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        try {
+            NotificationManagerCompat.from(context).notify(NOTIFICATION_ID,notif)
+        }catch (e:SecurityException){
+            Log.e("errornotif",e.message.toString())
+        }
+
+    }
+}
+
+class TodoWorker(context: Context, params:WorkerParameters):Worker(context,params){
+    override fun doWork(): Result {
+        val judul = inputData.getString("title").toString()
+        val msg = inputData.getString("message").toString()
+        NotificationHelper(applicationContext).creatNotification(judul,msg)
+        return Result.success()
+    }
+
 }
--- app/src/main/java/com/example/todoapp/view/CreateTodoFragment.kt
+++ app/src/main/java/com/example/todoapp/view/CreateTodoFragment.kt
@@ -1,5 +1,8 @@
 package com.example.todoapp.view
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,15 +10,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentCreateTodoBinding
 import com.example.todoapp.model.Todo
+import com.example.todoapp.util.NotificationHelper
+import com.example.todoapp.util.TodoWorker
 import com.example.todoapp.viewmodel.DetailTodoViewModel
+import java.util.concurrent.TimeUnit
 
 
-class CreateTodoFragment : Fragment() {
+class CreateTodoFragment : Fragment(), RadioClickListener,TodoEditClickListener {
 
     private lateinit var binding:FragmentCreateTodoBinding
     private lateinit var viewModel: DetailTodoViewModel
@@ -32,14 +42,62 @@ class CreateTodoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if(ActivityCompat.checkSelfPermission(requireContext(),Manifest.permission.POST_NOTIFICATIONS)!= PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    NotificationHelper.REQUEST_NOTIF)//arrayof ini bisa banyak permission kita minta (itu bisa dikoma sebelah notifications)
+            }
+        }
+
+        binding.todo = Todo("","",3,0)
+        binding.radiolistener = this
+        binding.addlistener = this
+
         viewModel = ViewModelProvider(this).get(DetailTodoViewModel::class.java)
 
-        binding.btnAdd.setOnClickListener{
-            val radio = view.findViewById<RadioButton>(binding.radioGroupPriority.checkedRadioButtonId)
-            val todo = Todo(binding.txtTitle.text.toString(), binding.txtNotes.text.toString(), radio.tag.toString().toInt(), 0)
-            viewModel.addTodo(todo)
-            Toast.makeText(context, "Todo Created",Toast.LENGTH_SHORT).show()
-            Navigation.findNavController(it).popBackStack()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults) //berisi informasi permision disetuju atau tidak
+        if(requestCode == NotificationHelper.REQUEST_NOTIF){
+            //cek jawaban pengguna
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val notif = NotificationHelper(requireContext())
+                notif.creatNotification("Todo Created","A new todo has been created!, Stay Focus!")
+            }
         }
+    }
+
+    override fun onTodoEditClick(v: View) {
+        //notification
+//            val notif = NotificationHelper(view.context)
+//            notif.creatNotification("Todo Created","A new todo has been created!, Stay Focus!")
+
+        val workRequest = OneTimeWorkRequestBuilder<TodoWorker>()
+            .setInitialDelay(20, TimeUnit.SECONDS)
+            .setInputData(
+                workDataOf(
+                    "title" to "Todo Created",
+                    "message" to "Stay Focus!")
+            ).build()
+        WorkManager.getInstance(requireContext()).enqueue(workRequest)
+
+
+//        val radio = view?.findViewById<RadioButton>(binding.radioGroupPriority.checkedRadioButtonId)
+//        val todo = Todo(binding.txtTitle.text.toString(), binding.txtNotes.text.toString(), radio?.tag.toString().toInt(), 0)
+//        viewModel.addTodo(todo)
+        viewModel.addTodo(binding.todo!!)
+        Toast.makeText(context, "Todo Created",Toast.LENGTH_SHORT).show()
+        Navigation.findNavController(v).popBackStack()
+    }
+
+    override fun onRadioClick(v: View) {
+        binding.todo!!.priority = v.tag.toString().toInt()
     }
 }
